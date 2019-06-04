dbe175847295c7c1c6891cf7d41aa98d57b248cf
Alex Creek <me@alexcreek.com>
2019-06-04 01:53:52 -0400

    parse json

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,11 @@
 FROM python:3.6
+
 WORKDIR /code
+
+# Putting here so pip install install isn't ran every time
+# the script is updated
 COPY requirements.txt .
+RUN pip3 install -r requirements.txt
+
 COPY creasy.py .
 CMD "./creasy.py"
--- creasy.py
+++ creasy.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3.6
 import os
+import requests
 
-print(__name__)
-print(os.environ['API_KEY'])
+def main():
+    """fetch and parse data"""
+
+    params = {'cx': '007775541357642725620:_e0fcphnv60',
+              'q': 'intitle:devops intext:remote',
+              'key': os.environ['API_KEY']}
+
+    resp = requests.get('https://www.googleapis.com/customsearch/v1', params=params)
+    data = resp.json()
+    for item in data['items']:
+        print(item['title'])
+        print(item['link'])
+        for metatag in item['pagemap']['metatags']:
+            if 'og:description' in metatag:
+                print(metatag['og:description'])
+
+if __name__ == '__main__':
+    main()
