e17de1f93a5f3f4548e56610f6ba38491d8c1553
Alex Creek <me@alexcreek.com>
2019-06-04 23:34:43 -0400

    use full pylintrc

--- peralta.py
+++ peralta.py
@@ -2,25 +2,35 @@
 import os
 import requests
 
-def main():
-    """fetch and parse data"""
+class Peralta():
+    def __init__(self,
+                 url='https://www.googleapis.com/customsearch/v1',
+                 cx='007775541357642725620:_e0fcphnv60',
+                 q='intitle:devops intext:remote'):
+        self.url = url
+        self.cx = cx
+        self.q = q
 
-    # TODO: add error handling
-    api_key = os.environ['API_KEY']
-    query_params = {'cx': '007775541357642725620:_e0fcphnv60',
-                    'q': 'intitle:devops intext:remote',
-                    'key': api_key}
+    def query(self):
+        """fetch data"""
 
-    # TODO: add error handling
-    resp = requests.get('https://www.googleapis.com/customsearch/v1', params=query_params)
+        # TODO: add error handling
+        api_key = os.environ['API_KEY']
+        query_params = {'cx': self.cx,
+                        'q': self.q,
+                        'key': api_key}
 
-    data = resp.json()
-    for item in data['items']:
-        print(item['title'])
-        print(item['link'])
-        for metatag in item['pagemap']['metatags']:
-            if 'og:description' in metatag:
-                print(metatag['og:description'])
+        # TODO: add error handling
+        resp = requests.get(self.url, params=query_params)
+
+        data = resp.json()
+        for item in data['items']:
+            print(item['title'])
+            print(item['link'])
+            for metatag in item['pagemap']['metatags']:
+                if 'og:description' in metatag:
+                    print(metatag['og:description'])
 
 if __name__ == '__main__':
-    main()
+    p = Peralta()
+    p.query()
